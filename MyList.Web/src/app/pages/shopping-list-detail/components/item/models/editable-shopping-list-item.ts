10b89604a9bd29b036a93df97d84c44f3da9da8f
Connor Lynch <clynch9145@gmail.com>
2023-12-13 22:05:36 -0600

    Deleted old list detail component

--- MyList.Web/src/app/pages/shopping-list-detail/components/item/models/editable-shopping-list-item.ts
+++
@@ -1,7 +0,0 @@
-import { ShoppingList } from "src/app/models/shopping-list";
-
-export interface EditableShoppingListItem {
-  item: ShoppingList,
-  selected: boolean,
-  editing: boolean,
-}
--- MyList.Web/src/app/pages/shopping-list-detail/components/item/models/item-form-fields.ts
+++
@@ -1,4 +0,0 @@
-export enum ItemFormFields {
-  Checked = 'checked',
-  Name = 'name',
-}
